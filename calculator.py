2fddaa1428f6b8e199d16732d665115146b2e064
Erenalp Yılmaz <erenalp@alpbilisim.com.tr>
2024-07-29 23:16:18 +0300

    Hatalar giderildi. İşlemleri kontrol etmesi için yeni fonksiyonlar oluşturuldu.

--- calculator.py
+++ calculator.py
@@ -11,25 +11,53 @@ def carp(a, b):
 def bol(a,b):
     return a/b
 
-sayi1 = float(input("1. sayıyı giriniz : "))
-sayi2 = float(input("2. sayıyı giriniz : "))
+def islem(islem):
+    sonuc = 0
+    match islem:
+        case "+":
+            sonuc = topla(sayi1, sayi2)
+            print(f"Sonuc = {sonuc}")
+        case "-":
+            sonuc = cikar(sayi1, sayi2)
+            print(f"Sonuc = {sonuc}")
+        case "*":
+            sonuc = carp(sayi1, sayi2)
+            print(f"Sonuc = {sonuc}")
+        case "/":
+            sonuc = bol(sayi1, sayi2)
+            print(f"Sonuc = {sonuc}")
 
-islem = str(input("Yapmak istediğiniz işlemi seçiniz +-*/  :"))
-sonuc = 0
+def tekrar(islemStr):
+    tekrarSayisi = 0
+    sonuc = ""
+    for i in "+-*/":
+        if islemStr == i:
+            tekrarSayisi +=1
+            sonuc +=""
+    if tekrarSayisi ==1 :
+        islem(islemStr)
+    else :
+        print("Yapmak istediğiniz işlemi yanlış belirttiniz . ")
 
-match islem :
-    case "+":
-        sonuc = topla(sayi1,sayi2)
-        print(f"Sonuc = {sonuc}")
-    case "-":
-        sonuc = cikar(sayi1,sayi2)
-        print(f"Sonuc = {sonuc}")
-    case "*":
-        sonuc = carp(sayi1,sayi2)
-        print(f"Sonuc = {sonuc}")
-    case "/":
-        sonuc = bol(sayi1,sayi2)
-        print(f"Sonuc = {sonuc}")
+
+try :
+    sayi1 = float(input("1. sayıyı giriniz : "))
+    sayi2 = float(input("2. sayıyı giriniz : "))
+    islemStr = str(input("Yapmak istediğiniz işlemi seçiniz +-*/  :"))
+    tekrar(islemStr)
+except :
+    print("Hatalı işlem yaptınız.")
+
+
+
+
+
+
+
+
+
+
+
 
 
 
